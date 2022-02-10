838f8b1a77a66f5cb15ea00f089b53927ec4837d
NoelCanMen <99173650+NoelCanMen@users.noreply.github.com>
2022-02-10 10:22:04 +0100

    Add files via upload

--- ProgramacionFuncional/src/ListaFuncional.java
+++ ProgramacionFuncional/src/ListaFuncional.java
@@ -1,10 +1,15 @@
+import java.util.List;
 
 public class ListaFuncional {
 
 	public static void main(String[] args) {
 		
 		
+		List<Integer> numeros = List.of(18,8,2,20,7,14,22,86,12);
 		
+		Long filtro =numeros.stream().filter(n->n>10).count();
+		
+		System.out.println(filtro);
 
 	}
 
